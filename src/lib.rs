c397a19282b8712e7e951c26d42ad9d48c148624
lash <dev@holbrook.no>
2024-06-29 22:48:06 +0100

    Module selection, default test guard

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,19 @@
 use std::collections::HashMap;
+use std::hash::Hasher;
+use std::hash::Hash;
 use std::iter::Iterator;
 use feed_rs::model::Entry;
 
 pub struct Sequencer {
-    pub items: HashMap<u32, Entry>,
+    pub items: HashMap<u32, Vec<u8>>,
     item_keys: Vec<u32>,
     crsr: usize,
+}
+
+pub struct SequencerEntry<'a> {
+    entry: Entry,
+    summer: &'a dyn Hasher,
+    digest: Vec<u8>,
 }
 
 impl Sequencer {
@@ -16,15 +24,50 @@ impl Sequencer {
             item_keys: Vec::<u32>::new(),
         }
     }
+
+    pub fn add<H: Hash>(&self, entry: SequencerEntry) {
+//        entry.hash(&self, entry);
+    }
 }
 
+impl<'a> SequencerEntry<'a> {
+    pub fn new(entry: Entry, summer: &'a dyn Hasher) -> SequencerEntry<'a> {
+        SequencerEntry {
+            entry: entry,
+            summer: summer,
+            digest: Vec::new(),
+        }
+    }
+}
+
+//impl<'a> Hash for SequencerEntry<'a> {
+//    fn hash(&self) -> u64 {
+//        Vec::<u64>::new()
+//    }
+//}
+//
+
 impl Iterator for Sequencer {
-    type Item = Entry;
+    type Item = Vec<u8>;
 
     fn next(&mut self) -> Option<Self::Item> {
         let c: u32;
 
         c = self.item_keys[self.crsr];
         return Some(self.items[&c].clone());
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use rs_sha512::Sha512Hasher;
+    use super::SequencerEntry;
+    use feed_rs::model::Entry;
+
+    #[test]
+    fn test_entry() {
+        let h = Sha512Hasher::default();
+        let src = Entry::default();
+        let entry = SequencerEntry::new(src, &h);
     }
 }
